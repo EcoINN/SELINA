7120ea2effc75a9d70a639e15a19e03897df9a1f
M. Verónica <mv.arambula13@gmail.com>
2022-10-03 14:44:43 +0200

    getting tweets

--- Twitter.R
+++ Twitter.R
@@ -7,12 +7,11 @@
 
 
 
-
 #### preparation of the script ####
 
 
 # set folder
-setwd("C:/Ecostack/Selina/output")
+setwd("C:/Ecostack/Selina/selina/output")
 
 # load the required packages
 
@@ -20,30 +19,46 @@ if( !("rjson" %in% installed.packages()[,1]))
   install.packages("rjson")
 library(rjson)
 
-if( !("dplyr" %in% installed.packages()[,1]))
-  install.packages("dplyr")
-library(dplyr)
+if( !("academictwitteR" %in% installed.packages()[,1]))
+  install.packages("academictwitteR")
+library(academictwitteR)
 
-if( !("httr" %in% installed.packages()[,1]))
-  install.packages("httr")
-library(httr)
-
-if( !("rtweet" %in% installed.packages()[,1]))
-  install.packages("rtweet")
-library(rtweet)
+if( !("tidyverse" %in% installed.packages()[,1]))
+  install.packages("tidyverse")
+library(tidyverse)
 
 
 
 #### connect to twitter ####
 
-# connect to twitter
-auth_setup_default()
+
+# json file 
+tokens <- fromJSON(file= "C:/Ecostack/Selina/selina/keys/TwitterKeys.json")
+
+# set bearer toke
+bearer_token <- tokens$Bearer
+
 
 #### search tweets ####
 
-# create a df 
-# this is just a test 
-df <- search_tweets2(c("#Malta", "#nature"),
-                    since_id="2022-09-21", max_id="2022-09-26",
-                    n=10, include_rts=FALSE)
 
+# query
+query <- build_query(query = "#malta #nature #travel", 
+                     is_retweet = FALSE,
+                     has_media = TRUE,
+                     has_images = TRUE)
+
+# get tweets
+tweets <-  get_all_tweets(query = query,
+                          start_tweets = "2021-01-01T00:00:00Z",
+                          end_tweets = "2021-12-31T00:00:00Z",
+                          data_path = "C:/Ecostack/Selina/selina/output",
+                          n = 10,
+                          bearer_token = bearer_token)
+
+View(tweets)
+
+
+#### inspect data ####
+
+tw_json <- bind_tweets(data_path = "C:/Ecostack/Selina/selina/output")
