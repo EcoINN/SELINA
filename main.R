d8f63a63d92eee611209421f37cefcdeb49385fb
M. Verónica <mv.arambula13@gmail.com>
2023-02-14 15:32:01 +0100

    appllied the functions

--- main.R
+++ main.R
@@ -11,8 +11,10 @@ library(rjson)
 library(jsonlite) 
 library(academictwitteR)
 library(tidytext)
+library(ggraph)
+library(tidyverse)
 
-
+# Prepare script
 # Loading supporting r-scripts
 invisible(sapply(list.files('./src', full.names = T), source))
 
@@ -36,21 +38,30 @@ keywords <- c('malta', 'gozo', 'comino', 'island', 'visitmalta', 'mymalta',
 start_date <- "2015-01-01T00:00:00Z"
 end_date <- "2022-12-31T00:00:00Z"
 
-# Connect to twitter and get tweets
+
+# Connect to the twitter API and
+# Get tweets
 tweets <- get_tweets(json_file, keywords, country, start_date, end_date, datdir)
 View(tweets)
 
 # Save as df
 df <- data.frame(tweets)
 
+# df preprocessing
+df_mt <- preprocess(df)
+df_mt <- df_clean(df_mt)
+
+# save as xls
+write.xlsx(df_mt, "./output/tweets_df.xlsx")
 
 
 # Twitter analysis
-# df preprocessing
-df_mt <- common_words(df)
+# Explore common words found on tweets, and plot
+c_words <- common_words(df_mt)
+plot_words(c_words, x = 'Count', y = 'Unique words' ,
+           title = 'Count of unique words found in tweets', 20)
 
-# Plot words
-plot_words(df_mt, x = 'Count', y = 'Unique words' , 
-           title = 'Count of unique words found in tweets')
-
-
+# Paired word analysis, and plot
+p_analysis <- tweet_bigrams(df_mt)
+plot_paired_words(p_analysis, title="Paired word analysis", 
+                  subtitle="Twitter ", 20)
