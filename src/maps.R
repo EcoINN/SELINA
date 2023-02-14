16efdbe0157de22c6cdbb4addc88a070f2adf132
M. Verónica <mv.arambula13@gmail.com>
2023-02-14 16:34:28 +0100

    added an object

--- src/maps.R
+++ src/maps.R
@@ -1,5 +1,5 @@
 #' ---
-#' Title: "JSON Functions"
+#' Title: "Map function (leaflet)"
 #' Author: EcoINN
 #' Date: "December 2022"
 #' Output: Map function
@@ -7,37 +7,20 @@
 
 
 # This function 
-maps <- function(tweet_data) {
-  # cleanup & and filter
-  mt_tweets <- tweet_data %>%
-    mutate(geo.coordinates = gsub("\\)|c\\(", "", geo.coordinates)) %>%
-    separate(geo.coordinates, c("long", "lat"), sep = ", ") %>%
-    mutate_at(c("lat", "long"), as.numeric) 
-  
-  # create basemap of the globe
-  world_basemap <- ggplot() +
-    borders("world", colour = "gray85", fill = "gray80")
-  
-  world_basemap
-  
+maps <- function(df) {
   # remove na values
-  tweet_locations <- tweet_data %>%
+  tweet_locations <- df %>%
     na.omit()
-  
   head(tweet_locations)
   
-  # plot the data
-  world_basemap +
-    geom_point(data = tweet_locations, aes(x = long, y = lat),
-               colour = 'purple', alpha = .5) +
-    scale_size_continuous(range = c(1, 8),
-                          breaks = c(250, 500, 750, 1000)) +
-    labs(title = "Tweet Locations")
-  
-  
+  tw_locations <- tweet_locations %>% rename("long" = "coordinates.1",
+                                             "lat" = "coordinates.2")
   # plot points on top of a leaflet basemap
-  site_locations <- leaflet(tweet_locations) %>%
-    addTiles() %>%
-    addCircleMarkers(lng = ~long, lat = ~lat, popup = ~tweet_text,
+  
+  site_locations_base <- leaflet(tw_locations) %>%
+    addProviderTiles("CartoDB.Positron") %>%
+    addCircleMarkers(lng = ~long, lat = ~lat, popup = ~text,
                      radius = 3, stroke = FALSE)
+  
+  return(site_locations_base)
 }
