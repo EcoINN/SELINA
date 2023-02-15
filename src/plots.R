6794a0f7c2458b09c30fe31b1ee8d0b887eeb189
M. Verónica <mv.arambula13@gmail.com>
2023-02-15 12:03:10 +0100

    function documentation

--- src/plots.R
+++ src/plots.R
@@ -8,16 +8,22 @@
 
 maps <- function(df) {
   # This functions useas leaflet to map tweets
-  # remove na values
+  #
+  # Args:
+  #    df: A data frame with long/lat data
+  #
+  # Returns: 
+  #    A map of all tweets in the df
+  #
+  # Remove na values
   tweet_locations <- df %>%
     na.omit()
   head(tweet_locations)
   
   tw_locations <- tweet_locations %>% rename("long" = "coordinates.1",
                                              "lat" = "coordinates.2")
-  # plot points on top of a leaflet basemap
-  
-  site_locations_base <- leaflet(tw_locations) %>%
+  # Plot points on top of a leaflet basemap
+    site_locations_base <- leaflet(tw_locations) %>%
     addProviderTiles("CartoDB.Positron") %>%
     addCircleMarkers(lng = ~long, lat = ~lat, popup = ~text,
                      radius = 3, stroke = FALSE)
