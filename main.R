d7a86c9938918b9aee54c53468dba0da54f4f63d
M. Verónica <mv.arambula13@gmail.com>
2023-02-22 14:22:15 +0100

    modified the structure of the script because of the change in the df function

--- main.R
+++ main.R
@@ -10,13 +10,15 @@
 library(rjson) 
 library(jsonlite) 
 library(academictwitteR)
+library(readxl)
 library(tidytext)
 library(ggraph)
+library(igraph)
 library(tidyverse)
 library(leaflet)
 
 
-# Prepare script
+# Prepare script ---------------------------
 # Loading supporting r-scripts
 invisible(sapply(list.files('./src', full.names = T), source))
 
@@ -24,41 +26,50 @@ invisible(sapply(list.files('./src', full.names = T), source))
 datdir <- 'data/'
 outdir <- 'output/'
 
-# Data needed
-json_file <- "./keys/TwitterKeys.json" # Twitter keys
-keywords <- fromJSON(file = "./keys/Keywords.json") # Twitter keywords
+# Data needed for Twitter
+keys <- "./keys/TwitterKeys.json" 
+keywords <- fromJSON(file = "./keys/Keywords.json")
 country <- "MT"
 start_date <- "2015-01-01T00:00:00Z"
 end_date <- "2022-12-31T00:00:00Z"
 
 
-# Connect to the twitter API and
+# Connect to the twitter API ---------------------------
 # Get tweets
-tweets <- get_tweets(json_file, keywords, country, start_date, end_date, datdir)
+tweets <- get_tweets(keys, keywords, country, start_date, end_date, datdir)
 View(tweets)
 
-# Save as df
-df <- data.frame(tweets)
-
-# df preprocessing
-df_mt <- preprocess(df)
-df_mt <- df_clean(df_mt)
-
-# save as xls
-write.xlsx(df_mt, "./output/tweets_df.xlsx")
+# Read json files
+tweets <- rjson("./data", "^data.*$")
+View(tweets)
 
 
-# Twitter analysis
+# Pre-processing ---------------------------
+# df preparation
+df <- process_tweets(tweets)
+View(df)
+
+# plot georeferenced tweets
+tw_map <- maps(df)
+tw_map
+
+
+# Twitter filtering ---------------------------
+
+
+
+
+# Twitter analysis ---------------------------
 # Explore common words found on tweets, and plot
-c_words <- common_words(df_mt)
+c_words <- common_words(df)
 plot_words(c_words, x = 'Count', y = 'Unique words' ,
            title = 'Count of unique words found in tweets', 20)
 
 # Paired word analysis, and plot
-p_analysis <- tweet_bigrams(df_mt)
+p_analysis <- tweet_bigrams(df)
 plot_paired_words(p_analysis, title="Paired word analysis", 
                   subtitle="Twitter ", 20)
 
-# plot coordinates
-tw_map <- maps(df_mt)
-tw_map
+
+
+
