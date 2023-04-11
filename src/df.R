72a694b90d1a321462e3dcbd5fe581646e26fb58
M. Verónica <mv.arambula13@gmail.com>
2023-04-11 13:01:54 +0200

    Added two new functions

--- src/df.R
+++ src/df.R
@@ -1,5 +1,5 @@
 #' ---
-#' Title: "Tweets preprocessing"
+#' Title: "Tweets processing"
 #' Author: EcoINN
 #' Date: "February 2023"
 #' Output: data frame
@@ -36,7 +36,7 @@ process_tweets <- function(tweets_df) {
     distinct(text, .keep_all=TRUE) %>%
     mutate(url1 = stringr::str_extract(text, "(https?://t\\.co/[^[:space:]]+)")) %>%
     mutate(text = gsub("(https?://t\\.co/[^[:space:]]+)", "", text)) %>%
-    filter(lang == "en") %>%
+    # filter(lang == "en") %>%
     mutate(text = gsub("&amp;", "and", text)) %>%
     select(-url1) %>%
     mutate(hashtags = sapply(str_extract_all(text, "#\\w+"), 
@@ -44,6 +44,62 @@ process_tweets <- function(tweets_df) {
     mutate(hashtags = ifelse(hashtags == "", NA , hashtags))
   
   return(tweets)
+}
+
+
+clean_text <- function(text) {
+  # Cleans and preprocess the text data.
+  #
+  # Args:
+  #    The df column containing the tweets text
+  #
+  # Returns: 
+  #    A df with a clean text
+  #
+  # Convert text to lowercase
+  text <- tolower(text)
+  # Remove URLs, mentions, and hashtags
+  text <- gsub("http[^[:blank:]]*|@[^[:blank:]]*|#\\S+", "", text)
+  # Remove emojis and other Unicode symbols, except spaces
+  text <- stri_replace_all_regex(text, "[\\p{So}\\p{Cs}\\p{Cn}]+", "")
+  # Remove punctuation and numbers
+  text <- gsub("[[:punct:]]|[[:digit:]]", "", text)
+  # Remove extra whitespace
+  text <- gsub("\\s+", " ", text)
+  # Remove leading and trailing whitespace
+  text <- str_trim(text)
+  return(text)
+}
+
+
+process_urls <- function(df) {
+  # Takes a data frame as input, separates multiple URLs in the expanded_url 
+  # column into different columns, and filters out the URLs that don't have 
+  # the word 'photo' in them.
+  #
+  # Args:
+  #    A df
+  #
+  # Returns: 
+  #    A df with filtered URLs
+  #
+  # Split the 'expanded_url' column by "," and create new columns
+  df <- df %>%
+    separate(expanded_url, into = paste0("url_", 1:5), sep = ",", 
+             remove = FALSE, extra = "merge", fill = "right") %>%
+    select(-expanded_url)
+  
+  # Create a function to filter out URLs without the word 'photo'
+  filter_photo_urls <- function(url_col) {
+    ifelse(grepl("photo", url_col, ignore.case = TRUE), url_col, NA)
+  }
+  
+  # Apply the function to each new URL column
+  for (col_name in colnames(df)[startsWith(colnames(df), "url_")]) {
+    df[[col_name]] <- sapply(df[[col_name]], filter_photo_urls)
+  }
+  
+  return(df)
 }
 
 
